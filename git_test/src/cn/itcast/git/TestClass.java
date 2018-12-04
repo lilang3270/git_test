52fd695550ab840924e01a4da81c2e53d39f29bf
zhangsan <zhangsan@163.com>
2018-12-04 18:54:53 +0800

    添加本地工程到git

---
+++ git_test/src/cn/itcast/git/TestClass.java
@@ -0,0 +1,13 @@
+package cn.itcast.git;
+
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("hello,git_project");
+        System.out.println("hello,git_project");
+        System.out.println("hello,git_project");
+        System.out.println("hello,git_project");
+        System.out.println("hello,git_project");
+        System.out.println("hello,git_project");
+        System.out.println("hello,git_project");
+    }
+}
